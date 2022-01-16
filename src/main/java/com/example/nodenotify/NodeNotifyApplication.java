3904716ead6e4d7eb9c36af63611d8d74e66c36a
muskaplan <muskaplan@innova.com.tr>
2022-01-16 15:15:53 +0300

    Initial commit

---
+++ src/main/java/com/example/nodenotify/NodeNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.nodenotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodeNotifyApplication.class, args);
+    }
+
+}
